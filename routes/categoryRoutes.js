483bb968e40ab32a36bc8dce85882f17aeb12139
Attainers <152283978+Fadlyevan@users.noreply.github.com>
2025-09-22 21:21:08 +0700

    Delete routes directory

--- routes/categoryRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { CategoryController } from "../src/controllers/categoryController.js";
-const router = express.Router();
-router.post("/", CategoryController.create);
-router.get("/", CategoryController.getAll);
-router.get("/:id", CategoryController.getById);
-router.put("/:id", CategoryController.update);
-router.delete("/:id", CategoryController.remove);
-export default router;
